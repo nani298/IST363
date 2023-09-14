f327f5dda997d3ca6e614726915603cecb81e55a
nani298 <95499532+nani298@users.noreply.github.com>
2023-09-14 18:21:39 -0400

    attendance 9/14

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -51,9 +51,171 @@ const coffeeObHeadline =document.createElement("h2");
 coffeeObHeadline.textContent = coffee.name;
 document.body.appendChild(coffeeObHeadline);
 
-const coffees = ["Mocha","Latte","Espresso"];
+const coffees = [
+    {
+        name:"Mocha",
+        price:3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName:"coffee.jpg",
+            width:630,
+            height:630,
+            altText:"A cup of coffee"
+        }
+    },
+     {
+        name:"Latte",
+        price:3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName:"coffee.jpg",
+            width:630,
+            height:630,
+            altText:"A cup of coffee"
+        }
+    },
+     {
+        name:"Esspresso",
+        price:3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName:"coffee.jpg",
+            width:630,
+            height:630,
+            altText:"A cup of coffee"
+        }
+
+    },
+    {
+        name:"Mocha",
+        price:3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName:"coffee.jpg",
+            width:630,
+            height:630,
+            altText:"A cup of coffee"
+        }
+    },
+     {
+        name:"Latte",
+        price:3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName:"coffee.jpg",
+            width:630,
+            height:630,
+            altText:"A cup of coffee"
+        }
+    },
+     {
+        name:"Esspresso",
+        price:3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName:"coffee.jpg",
+            width:630,
+            height:630,
+            altText:"A cup of coffee"
+        }
+    },
+    {
+        name:"Mocha",
+        price:3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName:"coffee.jpg",
+            width:630,
+            height:630,
+            altText:"A cup of coffee"
+        }
+    },
+     {
+        name:"Latte",
+        price:3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName:"coffee.jpg",
+            width:630,
+            height:630,
+            altText:"A cup of coffee"
+        }
+    },
+     {
+        name:"Esspresso",
+        price:3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName:"coffee.jpg",
+            width:630,
+            height:630,
+            altText:"A cup of coffee"
+        }
+    },
+    {
+        name:"Mocha",
+        price:3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName:"coffee.jpg",
+            width:630,
+            height:630,
+            altText:"A cup of coffee"
+        }
+    },
+     {
+        name:"Latte",
+        price:3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName:"coffee.jpg",
+            width:630,
+            height:630,
+            altText:"A cup of coffee"
+        }
+    },
+     {
+        name:"Esspresso",
+        price:3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName:"coffee.jpg",
+            width:630,
+            height:630,
+            altText:"A cup of coffee"
+        }
+    },
+
+
+];
 //console.log(coffees[0]);
 
 coffees.forEach(function(coffee) {
-    console.log(coffee);
-});
+    const {name,price,description,image}=coffee;
+
+    const coffeeArticle = document.createElement("article");
+
+    const coffeeImage = document.createElement("img");
+    coffeeImage.src =`images/${image.fileName}`;
+    coffeeImage.width = image.width;
+    coffeeImage.height =image.height;
+    coffeeImage.alt = image.altText;
+
+    const coffeeName = document.createElement("h2");
+    coffeeName.textContent = name;
+
+    const coffeePrice = document.createElement("h3");
+    coffeePrice.textContent = price;
+
+    const coffeeDescription = document.createElement("p");
+    coffeeDescription.textContent = description;
+
+    coffeeArticle.appendChild(coffeeImage);
+    coffeeArticle.appendChild(coffeeName);
+    coffeeArticle.appendChild(coffeePrice);
+    coffeeArticle.appendChild(coffeeDescription);
+
+    document.body.appendChild(coffeeArticle);
+    //console.log(coffee);
+});//end of coffee foreach method
+
